9a66d25f4e45eb49a3aaa9b834659c3a984c3e0c
ashy0611 <ashleedasilva.internship@gmail.com>
2026-01-16 11:30:25 +0530

    created sql files

---
+++ DATA_QUARIES/roles.sql
@@ -0,0 +1,25 @@
+Create table users 
+(
+	user_id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR(150) NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	role_id INT NOT NULL,
+	CONSTRAINT fk_user_role
+		FOREIGN KEY(role_id)
+		REFERENCES roles(role_id)
+		ON DELETE  RESTRICT
+);
+
+
+INSERT INTO users(name,email,password,role_id)
+VALUES 
+('Raju Singh','raju@xyz.com',crypt('password1234',gen_salt('bf')),1),
+
+('Sham Naik','sham@xyz.com',crypt('password1234',gen_salt('bf')),2),
+
+('Rinku Takur','rinku@xyz.com',crypt('password1234',gen_salt('bf')),3),
+
+('Ajay Bhatt','ajay@xyz.com',crypt('password1234',gen_salt('bf')),4),
+
+('Danish Khan','danish@xyz.com',crypt('password1234',gen_salt('bf')),5);
